32a0329d76f1d3c5b827a8aba8f5c8b0e8c363f1
kutar37 <kutar37@gmail.com>
2019-01-01 20:17:40 +0900

    1페이지에 파라미터 받지 않도록 수정

--- src/main/java/com/kutar/common/Criteria.java
+++ src/main/java/com/kutar/common/Criteria.java
@@ -3,20 +3,39 @@ package com.kutar.common;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
-@Setter
 @ToString
+@Slf4j
 public class Criteria {
 	
 	private int pageNum;
 	private int amount;
 	private int rowStart;
 	
+	public Criteria() {
+		this(1);
+	}
+	
 	public Criteria(int pageNum) {
 		this.pageNum = pageNum;
 		this.amount = 10;
-		this.rowStart = (pageNum-1) * this.amount;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+		if(pageNum > 1) {
+			this.rowStart = (pageNum-1) * this.amount;
+		}
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public void setRowStart(int rowStart) {
+		this.rowStart = rowStart;
 	}
 	
 }
