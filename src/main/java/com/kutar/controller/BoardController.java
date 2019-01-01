3e15ca6f744d93a34a2900c7a7ff382bb30411ba
kutar37 <kutar37@gmail.com>
2019-01-01 16:44:07 +0900

    controller 계층 CRUD / mvc 단위테스트 추가

---
+++ src/main/java/com/kutar/controller/BoardController.java
@@ -0,0 +1,62 @@
+package com.kutar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kutar.model.BoardVO;
+import com.kutar.service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/board/*")
+@Slf4j
+public class BoardController {
+
+	@Autowired
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		model.addAttribute("list",service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("새 게시물 등록 : {}", board);
+		
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("게시물 조회 : {}", bno);
+		model.addAttribute("board",service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("게시물 수정 : {}", board);
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("게시물 삭제 : {}", bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+}
