744d75932d4658921ac8c232ff17867a8c8a421a
kutar37@gmail.com <kutar37@gmail.com>
2019-02-15 10:23:58 +0900

    댓글기능 Mapper 추가

---
+++ src/main/java/com/kutar/mapper/ReplyMapper.java
@@ -0,0 +1,24 @@
+package com.kutar.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.kutar.common.Criteria;
+import com.kutar.model.ReplyVO;
+
+public interface ReplyMapper {
+
+	public int insert(ReplyVO reply);
+	
+	public ReplyVO read(Long rno);
+	
+	public int update(ReplyVO reply);
+	
+	public int delete(Long rno);
+	
+	public List<ReplyVO> getListWithPaging(
+			@Param("cri") Criteria cri,
+			@Param("bno") Long bno);
+	
+}
---
+++ src/test/java/com/kutar/mapper/ReplyMapperTests.java
@@ -0,0 +1,67 @@
+package com.kutar.mapper;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.kutar.common.Criteria;
+import com.kutar.model.ReplyVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Slf4j
+public class ReplyMapperTests {
+	
+	@Autowired
+	private ReplyMapper mapper;
+	
+	private Long[] bnoArr = {32760L, 32755L, 32754L, 24562L, 24561L};
+	
+	@Test
+	public void testMapper() {
+		log.info(mapper.toString());
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i->{
+			ReplyVO reply = new ReplyVO();
+			reply.setBno(bnoArr[i % 5]);
+			reply.setReply("댓글 테스트: " + i);
+			reply.setReplyer("댓글자: " +i);
+			mapper.insert(reply);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		log.info(mapper.read(5L).toString());
+	}
+	
+	@Test
+	public void testDelete() {
+		mapper.delete(4L);
+	}
+	
+	@Test
+	public void testUpdate() {
+		ReplyVO reply = mapper.read(5L);
+		reply.setReply("업데이트된 댓글");
+		mapper.update(reply);
+	}
+	
+	@Test
+	public void testList() {
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		replies.forEach(reply->log.info(reply.toString()));
+	}
+	
+}
