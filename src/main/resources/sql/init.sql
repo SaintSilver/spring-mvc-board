5a22ea4f21bb09279fb0a465f4f1d659c8b47466
kutar37@gmail.com <kutar37@gmail.com>
2019-02-15 09:57:45 +0900

    댓글 테이블 추가

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -23,3 +23,18 @@ INSERT INTO
 SELECT 
 	title, content, writer
 FROM board.tbl_board;
+
+CREATE TABLE `board`.`tbl_reply` (
+  `rno` INT NOT NULL AUTO_INCREMENT,
+  `bno` INT NOT NULL,
+  `reply` VARCHAR(1000) NOT NULL,
+  `replyer` VARCHAR(50) NOT NULL,
+  `replyDate` DATETIME NULL DEFAULT NOW(),
+  `updateDate` DATETIME NULL DEFAULT NOW(),
+  PRIMARY KEY (`rno`),
+  INDEX `fk_reply_board_idx` (`bno` ASC) VISIBLE,
+  CONSTRAINT `fk_reply_board`
+    FOREIGN KEY (`bno`)
+    REFERENCES `board`.`tbl_board` (`bno`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
